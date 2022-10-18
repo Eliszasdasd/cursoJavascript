180499a8487dc68403c9108d9527d914d4e34c6a
Lisza <liszalobo14@gmail.com>
2022-10-18 10:28:34 -0500

    inicio de la primera clase de javascript

---
+++ 1.tiposDeDato/index.js
@@ -0,0 +1,13 @@
+//tipos de datos
+//datos primitivos
+//3 tipos de datos
+"nombre" //string, texto
+"9" //string, texto
+"hola soy un saludo" //string, texto
+"false" //string, texto
+'soy una mariposa' //el dato string puede tener comillas dobles //comillas simples (TECLA ?)
+`soy otro dato` //string observacio //literal string (P)
+15 //tipo de dato numerico entero
+12.3 //tipo de dato numerico decimal
+false //tipo de dato booleano falso
+true //tipo de dato booleano verdadero
---
+++ 2.variables/index.js
@@ -0,0 +1,45 @@
+//variables
+//una variable es un espacio donde almacenamos 
+//un tipo de dato
+//para ser reultilizado
+
+//uso de variable
+//palabra reservada es una instruccion propia
+//del lenguaje de programacion
+
+//palabra reservada para declarar una variable
+//existen 2 
+ 
+//let
+ //para declarar una variable 
+ //se necesita de 2 cosas
+ //la palabra reservada y el nombre en donde quiero  almacenar el tipo de dato
+ let nobre_de_pais
+ var nombrePresidente
+ let EdadDeMiPerro
+ //var
+
+
+
+ //variables NO APTOS
+  //let @hola
+  //let 2hola
+  //let -nombre
+  //let x
+  //let uno
+
+ //proceso de asignacion
+ //el nombre de la variable, el sigo de asignacion
+ //y el tipo de dato a almacenar
+ nombre_de_pais='India'
+ //las variables son mutables
+ //que pueden ser reescritos
+ nombre_de_pais='Bolivia' //estoy reescribiendo el tipo de dato
+ nombre_de_pais=15 //estoy reescribiendo el tipo de dato
+ nombre_de_pais=true 
+
+console.log(nombre_de_pais)
+
+ //constantes
+ const numeroDni=123455
+ console.log(numeroDni)
