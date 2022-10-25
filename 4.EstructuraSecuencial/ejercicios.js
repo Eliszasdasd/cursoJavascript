02fd8913b3014ea1bb86cd076de7d70f836ed7e9
Lisza <liszalobo14@gmail.com>
2022-10-25 10:08:51 -0500

    Estructura secuncial y de decision

---
+++ 4.EstructuraSecuencial/ejercicios.js
@@ -0,0 +1,23 @@
+
+/Declaramos variables/
+var x=1;
+var y=2;
+var z;
+
+/realizamos operaciones aritmeticos/
+
+z=x+y;
+console.log("El resultado de la suma es:"+z)
+
+z=x-y;
+console.log("El resultado de la resta es:"+z)
+
+z=x*y;
+console.log("El resultado de la multiplicación es:"+z)
+
+z=x/y;
+console.log("El resultado de la división es:"+z)
+
+
+/* Se cuando un proceso o instrucciòn sigue o es seguido por otro proceso, esto se realiza de forma secuencial,
+asi la salida de uno es la entrada de otro y asi sucesivamente hasta el final del algoritmo.*/
---
+++ 5.EstructuraDecision/index.js
@@ -0,0 +1,19 @@
+/*la estructura de decisión
+seguira un camino distinto
+segun la condición*/
+
+/* Se utiliza para tomar desiciones lógicas, 
+se evalua una condición y dependiendo del resultado de la misma 
+que camino o acción se debe realizar
+se tiene 3 grupos de condiciones o estructuras selectivas, estos son:
+
+/* 1. condiciones simples.- solo vamos a tener un camino a relizar solo si se cumple una condición 
+si no el algorimo continua el fujo que tenga establecido, esto es para ejecutar una determinada 
+accion cuando se cumple una determinada condicion.
+  Para evaluar condiciones se usa la palabra SI, donde si el resultado de la condición es verdadera, se ejecutan 
+  las instrucciones dentro del condiones, si caso contrario el resultado es falso, entonces no se hace nada.*/
+
+/*2. condiciones dobles.- vamos a tener dos caminos por realizar, se realiza uno o se realiza el otro 
+  pero nunca se van a realizar los dos en la mism ejecución del algoritmo.*/
+
+/*3. condiciones multiples.- vamos a tener multiples caminos por realizar */
