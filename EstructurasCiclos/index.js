18998642fd63e6f67799969819f505558b27489a
Lisza <liszalobo14@gmail.com>
2022-11-07 20:29:45 -0500

    funciones

---
+++ EstructurasCiclos/index.js
@@ -0,0 +1,35 @@
+//Función Palindromo//
+
+function Palindromo(text) {
+  
+
+    return  text.split("").reverse().join("") === text ;
+  }
+  console.log(Palindromo("vaso")); 
+  console.log(Palindromo("ara")); 
+  console.log(Palindromo("omo")); 
+
+
+
+
+//Función Fibonacci//
+
+function fibonacci(n){
+    return n < 2? n: fibonacci(n-1) + fibonacci(n-2)
+  }
+  console.log(fibonacci(5))
+  console.log(fibonacci(7))
+  console.log(fibonacci(9))
+
+
+
+
+
+  //hacer una programa que muestre el factorial de un numero //
+
+const numero = 5; 
+let factorial = 1;
+for(let i = numero; i>0; i--){
+    factorial *= i;
+}
+console.log(factorial);
